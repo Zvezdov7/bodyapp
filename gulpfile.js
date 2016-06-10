710f551f4ab494efb2f56b07aea2ff78f1dfef15
Dmitry Zvezdov <zvezdov7@mail.ru>
2016-06-10 13:44:53 +0300

    added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,27 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util'),
-	coffee = require('gulp-coffee'),
-	concat = require('gulp-concat');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
+    concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
-var jsSource = [
-	'components/scripts/rclick.js',
-	'components/scripts/tagline.js',
-	'components/scripts/template.js',
-	'components/scripts/pixgrid.js'
+var jsSources = [
+  'components/scripts/rclick.js',
+  'components/scripts/pixgrid.js',
+  'components/scripts/tagline.js',
+  'components/scripts/template.js'
 ];
 
-gulp.task('log', function() {
-	gutil.log("It works");
-});
-
-gulp.task('coffee', function(){
-	gulp.src(coffeeSources)
-	  .pipe(coffee({bare: true})
-	  	.on('error', gutil.log))
-	  .pipe(gulp.dest('components/scripts'))
-
-
+gulp.task('coffee', function() {
+  gulp.src(coffeeSources)
+    .pipe(coffee({ bare: true })
+      .on('error', gutil.log))
+    .pipe(gulp.dest('components/scripts'))
 });
 
 gulp.task('js', function() {
-	gulp.src(jsSource)
-		.pipe(concat('script.js'))
-		.pipe(gulp.dest('builds/development/js'))
+  gulp.src(jsSources)
+    .pipe(concat('script.js'))
+    .pipe(browserify())
+    .pipe(gulp.dest('builds/development/js'))
 });
